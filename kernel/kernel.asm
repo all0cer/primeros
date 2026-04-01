3c5455f1ec562d8ef59f475e32816c63e4ddaa17
all0cer <italoalmeinasc@gmail.com>
2026-03-31 22:14:26 -0300

    feat: Add print string in kernel

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,16 +1,31 @@
 [BITS 16]
 [ORG 0x0000]
 
+mov ah, 0x0E
+        mov al, 'P'
+        int 0x10
+
 jmp OSmain
 
 BackWidth db 0         ; Variável: largura da tela (em colunas)
 BackHeight db 0        ; Variável: altura da tela (em linhas)
 Pagination db 0        ; Será usado futuramente!!
+KernelMessage db "Kernel em execucao", 0
 
 OSmain:
+    ; Configura a stack primeiro (Inline para não quebrar o ret)
+    cli                 ; Desativa interrupções durante a troca de stack
+    mov ax, 9000h
+    mov ss, ax
+    mov sp, 0FFFFh
+    sti                 ; Reativa interrupções
+
     call ConfigSegment
-    call ConfigStack
-    call TEXT.setVideoMode
+
+    call textsetVideoMode
+
+    jmp ShowMessage
+
 
 ; -----------------------------
 ; Configuração de segmentos
@@ -21,18 +36,10 @@ ConfigSegment:
 ret                    ; Retorna para quem chamou
 
 ; -----------------------------
-; Configuração da stack
-; -----------------------------
-ConfigStack:
-    mov ax, 9000h      ; Define o segmento da stack (0x9000)
-    mov ss, ax         ; SS = 0x9000 (segmento da stack)
-    mov sp, 0FFFFh      ; SP = 0xFFFF (topo da stack, cresce para baixo)
-ret                    ; Retorna
-
-; -----------------------------
 ; Configuração de vídeo (modo texto)
 ; -----------------------------
-TEXT.setVideoMode:
+textsetVideoMode:
+
     mov ah, 00h        ; Função 0 da BIOS de vídeo (int 10h) → setar modo de vídeo
     mov al, 03h        ; Modo 03h = texto 80x25 (modo padrão VGA)
     int 10h            ; Chama BIOS para aplicar o modo de vídeo
@@ -40,3 +47,44 @@ TEXT.setVideoMode:
     mov BYTE [BackWidth], 80   ; Armazena largura da tela (80 colunas)
     mov BYTE [BackHeight], 25  ; Armazena altura da tela (25 linhas)
 ret                    ; Retorna
+
+MoveCursor:
+    mov ah, 02h
+    mov bh, [Pagination]
+    inc dl
+    int 10h
+ret
+
+SetLineColumn:
+    mov dh, 3
+    mov dl, 2
+ret
+
+ShowMessage:
+    mov ah, 0x0E
+        mov al, 'H'
+        int 0x10
+    call SetLineColumn
+    call MoveCursor
+    mov si, KernelMessage
+    call MakeString
+    jmp END
+
+MakeString:
+    mov ah, 09h
+    mov bh, [Pagination]
+    mov bl, 40h
+    mov cx, 1
+    mov al, [si]
+    print:
+        int 10h
+        inc si
+        call MoveCursor
+        mov ah, 09h
+        mov al, [si]
+        cmp al, 0
+        jne print
+ret
+
+END:
+ jmp $
