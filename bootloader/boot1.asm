0559afdcd9c36fe0df3a71a11113c0fcbab81d35
all0cer <italoalmeinasc@gmail.com>
2026-03-31 22:15:40 -0300

    fix and feat: Add error check with "jc" and set disk type with value of BIOS set in "dl"

--- bootloader/boot1.asm
+++ bootloader/boot1.asm
@@ -1,14 +1,22 @@
 [BITS 16]
 [ORG 0x7C00]
 
-call PrepHelloWorld
+main:
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov [BOOT_DRIVE], dl    ; Salva o drive de boot passado pela BIOS
+
+    call PrepHelloWorld
 call PrintAll
 
 call PrepKernelPhrase
 call PrintAll
 
 call LoadingKernel
-
+ mov ah, 0x0E
+    mov al, 'J'
+    int 0x10
 jmp 0800h:0000h
 
 PrepHelloWorld:
@@ -35,15 +43,24 @@ LoadingKernel:
     mov ch, 0          ; cilindro (track) = 0
     mov cl, 2          ; setor = 2 (setor 1 é o bootloader, então começa no 2
     mov dh, 0          ; head (cabeça) = 0 (primeira cabeça do disco)
-    mov dl, 80h        ; drive = 0x80 (primeiro HD) | 0x00 seria disquete
+    mov dl, [BOOT_DRIVE] ; drive = recuperado da BIOS
     mov bx, 0800h      ; prepara segmento onde os dados serão carregados
     mov es, bx         ; ES = 0x0800 → segmento de destino na memória
     mov bx, 0000h      ; offset dentro do segmento (ES:BX → 0x0800:0000)
     int 13h            ; chama BIOS → lê o setor e coloca em ES:BX
+    jc DiskError
     ret                ; retorna da função
 
-FirstPhrase db "Iniciando processo de boot....", 0
-KernelPrepPhrase db "Carregando Kernel...", 0
+
+DiskError:
+    mov ah, 0x0E
+    mov al, 'E'
+    int 0x10
+
+
+FirstPhrase db "Iniciando processo de boot....", 0x0D, 0x0A, 0
+KernelPrepPhrase db "Carregando Kernel...", 0x0D, 0x0A, 0
+BOOT_DRIVE db 0
 times 510 - ($-$$) db 0
 dw 0xAA55
 
