89dc43c8e531ce916abd50a1ac74a84daf4a302b
all0cer <italoalmeinasc@gmail.com>
2026-05-18 15:25:25 -0300

    feat: Modify kernel asm to use monitor.lib

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,16 +1,12 @@
 [BITS 16]
 [ORG 0x0000]
 
-mov ah, 0x0E
-        mov al, 'P'
-        int 0x10
 
 jmp OSmain
 
-BackWidth db 0         ; Variável: largura da tela (em colunas)
-BackHeight db 0        ; Variável: altura da tela (em linhas)
-Pagination db 0        ; Será usado futuramente!!
-KernelMessage db "Kernel em execucao", 0
+; INCLUSIONS --------------------------------------
+%INCLUDE "hardware/monitor.lib"
+
 
 ; START KERNEL---------------------------------------------------------
 OSmain:
@@ -22,8 +18,10 @@ OSmain:
     sti                 ; Reativa interrupções
 
     call ConfigSegment
-    jmp ShowMessage
-
+    call VGA.SetVideoMode
+    call DrawBackGround
+    call EffectInit
+    jmp END
 ConfigSegment:
     mov ax, es         ; Copia o valor do segmento ES para AX
     mov ds, ax         ; Define DS = ES (alinha segmento de dados com ES)
@@ -31,30 +29,13 @@ ret                    ; Retorna para quem chamou
 ; -----------------------------------------------------------------------
 
 
-MoveCursor:
-    mov ah, 02h        ; Função BIOS: mover cursor
-    mov bh, [Pagination] ; Página de vídeo
-    inc dl             ; Move cursor uma coluna à direita
-    int 0x10           ; Aplica posição do cursor
-ret
 
-
-MakeString:
-    mov ah, 09h        ; Função BIOS: escrever caractere com atributo
-    mov bh, [Pagination] ; Página
-    mov bl, 20h        ; Atributo de cor
-    mov cx, 1          ; Quantidade de repetições
-    mov al, [si]       ; Caractere atual
-
-    print:
-        int 10h            ; Imprime caractere com cor
-        inc si             ; Avança para próximo caractere
-        call MoveCursor    ; Move cursor manualmente
-        mov ah, 09h        ; Reconfigura função (BIOS pode alterar AH)
-        mov al, [si]       ; Próximo caractere
-        cmp al, 0          ; Verifica fim da string (null terminator)
-        jne print          ; Se não for fim, continua loop
-ret
 
 END:
- jmp $
+   mov ah, 00h
+   int 16h
+   mov ax, 0040h
+   mov ds, ax
+   mov ax, 1234h
+   mov [0072h], ax
+   jmp 0FFFFh:0000h
